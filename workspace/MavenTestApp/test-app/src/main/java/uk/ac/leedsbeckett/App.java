efc05c073047f2882dac062d663ebe5fbdfe3421
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-03-06 20:14:33 +0000

    The Maven test app is set up using command line ✅

---
+++ workspace/MavenTestApp/test-app/src/main/java/uk/ac/leedsbeckett/App.java
@@ -0,0 +1,11 @@
+package uk.ac.leedsbeckett;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This is a test application. Built using Maven Command Line.");
+    }
+}
